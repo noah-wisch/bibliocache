e731297a6b79f966a75bcb752f3e14480a37a61d
Noah Wisch <noah.wisch@gmail.com>
2017-02-08 12:45:32 -0500

    Added module folders in JS and package.json

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,44 +1,38 @@
-// Import Gulp
-let gulp = require('gulp');
 
-// Import Gulp Plugins
-let sass = require('gulp-sass'); // download this npm plugin
-let browser = require('gulp-browser');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browser = require('gulp-browser');
+const strip = require('gulp-strip-comments');
 
-// Default Task
 gulp.task('default', ['html', 'css', 'js']);
 
-// Subtasks
-gulp.task('html', function() {
-	gulp.src('templates/*.html')
-		.pipe(gulp.dest('public/templates'));
-	
-	// copy index.html into the public/ directory
-	return gulp.src('index.html')
-		.pipe(gulp.dest('public/'));
-})
+gulp.task('html', function () {
+    gulp.src('templates/*.html')
+        .pipe(gulp.dest('public/templates'));
 
-gulp.task('css', function() {
-	// convert style.scss into style.css
-	// copy to public/
-	return gulp.src('scss/style.scss')
-		.pipe(sass()) // requires gulp-sass
-		.pipe(gulp.dest('public/'));
-})
+    return gulp.src('index.html')
+        .pipe(gulp.dest('public/'))
+        .pipe(strip.text());
+});
 
-gulp.task('js', function() {
-	// copy js file into public/
-	return gulp.src('js/app.js')
-		.pipe(browser.browserify()) // require() local files
-		.pipe(gulp.dest('public/'));
-})
+gulp.task('css', function () {
+    return gulp.src('scss/style.scss')
+        .pipe(sass())
+        .pipe(strip.text())
+        .pipe(gulp.dest('public/'));
+});
 
-// Watch files for changes
-gulp.task('watch', ['default'], function() {
-	// gulp.watch('files-to-watch', 'tasks to run')
-	gulp.watch('*.html', ['html']);
-	gulp.watch('templates/*.html', ['html']);
-	gulp.watch('scss/*.scss', ['css']);
+gulp.task('js', function () {
+    return gulp.src('js/app.js')
+        .pipe(browser.browserify())
+        .pipe(strip.text())
+        .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', ['default'], function () {
     gulp.watch('js/*.js', ['js']);
-	gulp.watch('js/*/*.js', ['js']);
+    gulp.watch('js/*/*.js', ['js']);
+    gulp.watch('scss/*.scss', ['css']);
+    gulp.watch('*.html', ['html']);
+    gulp.watch('templates/*.html', ['html']);
 });
