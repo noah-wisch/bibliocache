f0c290f9b5fe8da179eeca3b9094f62fb035a695
Margo Lanier <lanier.margo@gmail.com>
2017-02-08 15:02:29 -0500

    added basic geolocation service that logs position to console.

--- client/js/controllers/map.js
+++ client/js/controllers/map.js
@@ -2,5 +2,14 @@ module.exports = {
     name: 'MapController',
     func($scope) {
 		
+		let map;
+		function initMap() {
+			map = new google.maps.Map(document.querySelector('#sessionMap'), {
+				center: {lat: -34.397, lng: 150.644},
+				zoom: 8,
+			});
+		}
+		initMap();
+		
     },
 };
--- client/js/controllers/newSession.js
+++ client/js/controllers/newSession.js
@@ -1,6 +1,42 @@
 module.exports = {
     name: 'NewSessionController',
     func($scope) {
+		
+	
+		
+		function getUserLocation() {
+			// Initiate geolocation service
+			let geo = navigator.geolocation;
+			
+			if ("geolocation" in navigator) {
+				console.log('location available');
+			} else {
+				alert("Geolocation services are not supported by your browser."); 
+			}
+			
+			function success(position) {
+				let pos = position.coords;
+				
+				console.log('Your current position is:');
+				console.log(`Latitude : ${pos.latitude}`);
+				console.log(`Longitude: ${pos.longitude}`);
+				console.log(`More or less ${pos.accuracy} meters.`);
+			};
+			
+			function error(err) {
+				console.warn(`ERROR(${err.code}): ${err.message}`);
+			};
+			
+			let positionOptions = {
+				enableHighAccuracy: true,
+				timeout: 5000,
+				maximumAge: 0
+			};
+			
+			geo.getCurrentPosition(success, error, positionOptions);
+		};
+		getUserLocation();
+		
         /**
          * two text boxes
          * drop-down menu w/ genres 
