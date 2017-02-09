e00b8334c06bef436f8043a27058bea579bae344
Margo Lanier <lanier.margo@gmail.com>
2017-02-09 14:04:31 -0500

    working on watchPosition for geolocation.  logging google maps directions to console.

--- client/js/controllers/newSession.js
+++ client/js/controllers/newSession.js
@@ -8,23 +8,23 @@ module.exports = {
 			// Initiate geolocation service
 			let geo = navigator.geolocation;
 			
-			function success(position) {
+			function geo_success(position) {
 				let pos = position.coords;
 				console.log(`current position: [${pos.latitude}, ${pos.longitude}]`);
 				updateLocation(pos.latitude, pos.longitude);
 			};
 			
-			function error(err) {
+			function geo_error(err) {
 				console.warn(`ERROR(${err.code}): ${err.message}`);
 			};
 			
-			let positionOptions = {
+			let geo_options = {
 				enableHighAccuracy: true,
 				timeout: 5000,
 				maximumAge: 0
 			};
 			
-			geo.getCurrentPosition(success, error, positionOptions);
+			geo.getCurrentPosition(geo_success, geo_error, geo_options);
 		};
 		
 		// If user's location is available
--- client/js/services/location-service.js
+++ client/js/services/location-service.js
@@ -2,7 +2,13 @@ module.exports = {
 	name: 'LocationService',
 	
 	func($http) {
-		let currentPos = [0, 0];
+		//let currentPos = [undefined, undefined];
+		//let finalPos = [undefined, undefined];
+		
+		let currentPos = [35.227298, -80.8464868];
+		let finalPos = [35.226143, -80.852892];
+		
+		let directions = undefined;
 		
 		return {
 			getUserLocation() {
@@ -12,6 +18,19 @@ module.exports = {
 				currentPos = [lat, lng];
 				return currentPos;
 			},
+			getDirections() {
+				return $http.get(`https://maps.google.com/maps/api/directions/json?origin=${currentPos[0]},${currentPos[1]}&destination=${finalPos[0]},${finalPos[1]}&key=AIzaSyAoCv60nVilICtLnfFn7JMYvN_s04li5V0`).then(function(response) {
+					let dir = response.data.routes[0].legs[0];
+					console.log(dir);
+					
+					for (let i=0; i<dir.steps.length; i++) {
+						console.log(dir.steps[i].html_instructions);
+					}
+					
+					
+				});
+				
+			}
 		};
 	},
 	
