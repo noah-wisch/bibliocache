d67b93d8e5bf6222ba486be19539506c1414f1d3
Noah Wisch <noah.wisch@gmail.com>
2017-02-09 12:01:34 -0500

    Added regex to login & changed input types.

--- client/js/controllers/login.js
+++ client/js/controllers/login.js
@@ -1,6 +1,11 @@
 module.exports = {
     name: 'LoginController',
     func($scope) {
-		
+        $scope.emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        $scope.createAccount = () => {
+            console.log('test account')
+            // UserService.send($scope.username)
+            // UserService.send($scope.password)
+        }
     },
 };
