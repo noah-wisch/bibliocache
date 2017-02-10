bfbc3edc016340441fc74f08f59cc77f314f49f6
Kelsey <kpnewman2013@gmail.com>
2017-02-09 20:45:38 -0500

    reading level algorithm is working

--- src/test/java/com/theironyard/entities/BookTest.java
+++ src/test/java/com/theironyard/entities/BookTest.java
@@ -21,4 +21,19 @@ public class BookTest {
         assertEquals(11, Book.wordsPresent("The worst-case scenario is that you die. You'll be okay though."));
     }
 
+    @Test
+    public void sentencesTest()throws Exception {
+        assertEquals(3, Book.sentencesPresent("This is a test. This is a T.L.A. test. Now with a Dr. in it."));
+    }
+
+    @Test
+    public void readingLevelTest() throws Exception  {
+        assertEquals(9, Book.readingLevelOfBook("Harsh sounds of chains and pulleys, like the workings of an ancient steel factory, echoed through the room, bouncing off the walls with a hollow, tinny whine. The lightless elevator swayed back and forth as it ascended, turning the boy's stomach sour with nausea; a smell like burnt oil invaded his senses, making him feel worse. He wanted to cry, but no tears came; he could only sit there, alone, waiting."));
+        //excerpt from The Maze Runner
+        assertEquals(1, Book.readingLevelOfBook("If a hungry little traveler shows up at your house, you might want to give him a cookie. If you give him a cookie, he's going to ask for a glass of milk. He'll want to look in a mirror to make sure he doesn't have a milk mustache."));
+        //excerpt from If You Give a Mouse a Cookie
+        assertEquals(14, Book.readingLevelOfBook("It was a special pleasure to see things eaten, to see things blackened and changed. With the brass nozzle in his fists, with this great python spitting its venomous kerosene upon the world, the blood pounded in his head, and his hands were the hands of some amazing conductor playing all the symphonies of blazing and burning to bring down the tatters and charcoal ruins of history."));
+        //excerpt from Fahrenheit 451
+    }
+
 }
