f8b8a8fc39ef1732fb4c12ea4b266724137f895f
Margo Lanier <lanier.margo@gmail.com>
2017-02-14 12:32:53 -0500

    updated gulp file to watch assets directory

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -4,35 +4,43 @@ const sass = require('gulp-sass');
 const browser = require('gulp-browser');
 const strip = require('gulp-strip-comments');
 
+const build_path = '../build/resources/main/static';
+const src_path = '../src/main/resources/static';
+
 gulp.task('default', ['html', 'css', 'js']);
 
-gulp.task('html', function () {
+gulp.task('html', () => {
     gulp.src('templates/*.html')
-        .pipe(gulp.dest('public/templates'));
+        .pipe(gulp.dest(`${build_path}/templates`))
+		.pipe(gulp.dest(`${src_path}/templates`));
 	
 	gulp.src('assets/*')
-        .pipe(gulp.dest('public/assets'));
-
-    return gulp.src('index.html')
-        .pipe(gulp.dest('public/'))
-        .pipe(strip.text());
+        .pipe(gulp.dest(`${build_path}/assets`))
+		.pipe(gulp.dest(`${src_path}/assets`));
+	
+    return gulp.src('*.html')
+        .pipe(strip.text())
+        .pipe(gulp.dest(build_path))
+        .pipe(gulp.dest(src_path));
 });
 
-gulp.task('css', function () {
+gulp.task('css', () => {
     return gulp.src('scss/style.scss')
         .pipe(sass())
         .pipe(strip.text())
-        .pipe(gulp.dest('public/'));
+        .pipe(gulp.dest(build_path))
+        .pipe(gulp.dest(src_path));
 });
 
-gulp.task('js', function () {
+gulp.task('js', () => {
     return gulp.src('js/app.js')
         .pipe(browser.browserify())
         .pipe(strip.text())
-        .pipe(gulp.dest('public/'));
+        .pipe(gulp.dest(build_path))
+        .pipe(gulp.dest(src_path));
 });
 
-gulp.task('watch', ['default'], function () {
+gulp.task('watch', ['default'], () => {
     gulp.watch('js/*.js', ['js']);
     gulp.watch('js/*/*.js', ['js']);
     gulp.watch('scss/*.scss', ['css']);
