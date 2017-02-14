c5006679898a137dd47a24f9f5c6ba1d42edfba3
Margo Lanier <lanier.margo@gmail.com>
2017-02-14 15:12:44 -0500

    renders route with start and end points on map

--- client/js/controllers/map.js
+++ client/js/controllers/map.js
@@ -72,6 +72,26 @@ module.exports = {
 			});
 			Map.setStreetView(Street);
 			
+			// Display directions
+			let directionsService = new google.maps.DirectionsService;
+        	let directionsDisplay = new google.maps.DirectionsRenderer;
+			directionsDisplay.setMap(Map);
+			
+			function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+				directionsService.route({
+					origin: currentPos,
+					destination: destination,
+					travelMode: 'DRIVING'
+				}, function(response, status) {
+					if (status === 'OK') {
+						directionsDisplay.setDirections(response);
+					} else {
+						window.alert('Directions request failed due to ' + status);
+					}
+				});
+			};
+			calculateAndDisplayRoute(directionsService, directionsDisplay);
+			
 		};
 		initMap();
 		
