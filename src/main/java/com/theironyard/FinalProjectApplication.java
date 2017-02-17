15b0163acd2e98c71b4f02d75870576dc017b85e
Kelsey <kpnewman2013@gmail.com>
2017-02-17 11:56:41 -0500

    adding endpoint for homepage

--- src/main/java/com/theironyard/FinalProjectApplication.java
+++ src/main/java/com/theironyard/FinalProjectApplication.java
@@ -1,15 +1,11 @@
 package com.theironyard;
 
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.theironyard.services.BookSample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 
 @SpringBootApplication
 public class FinalProjectApplication {
