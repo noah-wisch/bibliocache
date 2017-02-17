a1c425607a27a5cae5f9519852c81a89c3e268e8
Margo Lanier <lanier.margo@gmail.com>
2017-02-17 14:35:28 -0500

    fixed get destination formula.

--- client/js/services/location-service.js
+++ client/js/services/location-service.js
@@ -22,16 +22,10 @@ module.exports = {
 				let latRange = maxRange * 0.015; // 1 mi lat = approx 1/69 deg lat
 				let lngRange = maxRange * 0.019; // 1 mi lng = approx 1/53 deg lng
 				
-				// get random number between 0 and 1
-				// maxRange * intensity(%) = randomRange
-				let intensity = () => {
-					return Math.random() * (.99 - .01) + .01;
-				};
-				
 				// Create rectangular bounds around current location to set a destination within the max range
 				// If range is 0.1 deg, the difference from current location will be +/- 0.05 deg for both lat and lng
-				let latDest = currentPos[0] + (intensity() * (1 - latRange/2));
-				let lngDest = currentPos[1] + (intensity() * (1 - lngRange/2));
+				let latDest = currentPos[0] + (Math.random() - 0.5) * latRange;
+				let lngDest = currentPos[1] + (Math.random() - 0.5) * lngRange;
 				
 				console.log('destination: [' + latDest, lngDest + ']');
 				endPos = [latDest, lngDest];
