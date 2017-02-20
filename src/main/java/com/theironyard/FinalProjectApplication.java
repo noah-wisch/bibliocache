11fad7da23cbb90ef62a3c78da4e39b3aef7f5ba
Kelsey <kpnewman2013@gmail.com>
2017-02-19 20:36:47 -0500

    fixed notloggedin

--- src/main/java/com/theironyard/FinalProjectApplication.java
+++ src/main/java/com/theironyard/FinalProjectApplication.java
@@ -1,11 +1,7 @@
 package com.theironyard;
 
-import com.theironyard.entities.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
